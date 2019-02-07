1720a692257401d54a6e9f042819be0494527472
engNagi <eng.m.nagi@gmail.com>
2019-02-07 17:59:32 +0100

    java_selenium_test3

--- src/test/java/example/newTest.java
+++ src/test/java/example/newTest.java
@@ -2,7 +2,6 @@ package example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
